7471de18962e9ddfbf014176c021b1f8c412dad0
SergioTaco <loyalman.seoeditor@gmail.com>
2021-10-06 22:13:45 +0300

    flask-3-handling-routes

--- python/hands-on/flask-03-handling-routes-and-if-for/Flask_if_for_structure/app.py
+++ python/hands-on/flask-03-handling-routes-and-if-for/Flask_if_for_structure/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask (__name__)
 
 @app.route('/')
 def head():
@@ -12,6 +12,6 @@ def mylist():
     names = ["Ahmet","Salih", "Tarkan", "Sergio", "Halil"]
     return render_template('body.html', object=names)
 
-
-if __name__ == ' __main__':
-    app.run(debug=True)
+if __name__ == '__main__':
+    #app.run(debug=True)
+    app.run(host='0.0.0.0', port=80)
